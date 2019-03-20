9f611b76e04220bc4d7c52c4618aa1a041b32416
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 17:28:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   selection = []
   while i < collection.length
-    if array[i] == collection
+    if collection[i] == true
       selection << yield(collection[i])
     end
       i+=1
